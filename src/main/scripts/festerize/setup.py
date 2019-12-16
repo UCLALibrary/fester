ae90d30930c2810f5152b1eca4714cff41d8ca0c
Hardy Pottinger <hpottinger@library.ucla.edu>
2019-12-16 09:13:42 -0600

    [IIIF-574] Add Festerize script for uploading CSVs to Fester (#40)
    * [IIIF-574] Add Festerize script for uploading CSVs to Fester

---
+++ src/main/scripts/festerize/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup (
+    name='Festerize',
+    version='0.1',
+    py_modules=['festerize'],
+    install_requires=[
+        'click',
+        'requests',
+        'datetime'
+    ],
+    entry_points='''
+        [console_scripts]
+        festerize=festerize:cli
+    ''',
+)
