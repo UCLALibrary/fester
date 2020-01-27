48aeab442d18e351a62ae44a8acdb48327204e1d
Kevin S. Clarke <ksclarke@ksclarke.io>
2020-01-27 15:18:07 -0500

    [IIIF-632] Update freelib dependency versions (#55)
    * [IIIF-632] Update freelib dependency versions
    * Update checkstyle rules to match latest Checkstyle
    * Fix post shutdown script

--- src/test/scripts/post-it-shutdown.sh
+++ src/test/scripts/post-it-shutdown.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
 
 # We have to provide a little wrapper script for exec-maven-plugin to background our job
-$* > /dev/null 2>&1 &
-echo $! > fester-it.pid
-exit 0
+kill `cat fester-it.pid`
+rm fester-it.pid
--- src/test/scripts/pre-it-startup.sh
+++ src/test/scripts/pre-it-startup.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
 # We have to provide a little wrapper script for exec-maven-plugin to background our job
+echo "$*"
 $* > /dev/null 2>&1 &
 echo $! > fester-it.pid
 exit 0
