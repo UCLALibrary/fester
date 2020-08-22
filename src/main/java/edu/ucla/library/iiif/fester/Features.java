834785df65478906ffdd7bc0a886b4b8043bc669
Kevin S. Clarke <ksclarke@ksclarke.io>
2020-08-22 01:34:22 -0400

    [IIIF-952] Update checkstyle and PMD rules

--- src/main/java/edu/ucla/library/iiif/fester/Features.java
+++ src/main/java/edu/ucla/library/iiif/fester/Features.java
@@ -6,18 +6,27 @@ import java.util.Map;
 import info.freelibrary.util.Logger;
 import info.freelibrary.util.LoggerFactory;
 
+/**
+ * A class containing the configurable features that are supported by the Fester application.
+ */
 public final class Features {
 
     public static final String BATCH_INGEST = "fester.batch.ingest";
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Features.class, Constants.MESSAGES);
 
-    private static final Map<String, String> FEATURE_NAMES = Map.of(BATCH_INGEST, LOGGER.getMessage(
-            MessageCodes.MFS_084));
+    private static final Map<String, String> FEATURE_NAMES =
+            Map.of(BATCH_INGEST, LOGGER.getMessage(MessageCodes.MFS_084));
 
     private Features() {
     }
 
+    /**
+     * Gets the display name of a feature.
+     *
+     * @param aFeatureKey A feature key
+     * @return The display name associated with that key
+     */
     public static String getDisplayName(final String aFeatureKey) {
         return FEATURE_NAMES.get(aFeatureKey);
     }
