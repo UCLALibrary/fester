5b0da0c2a418f3971a332301378769648388738d
Kevin S. Clarke <ksclarke@ksclarke.io>
2026-04-30 18:08:02 -0400

    Upgrade logging for bad URLs (#268)

--- src/main/java/edu/ucla/library/iiif/fester/verticles/V3ManifestVerticle.java
+++ src/main/java/edu/ucla/library/iiif/fester/verticles/V3ManifestVerticle.java
@@ -5,9 +5,7 @@ import static edu.ucla.library.iiif.fester.Constants.EMPTY;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -576,14 +574,12 @@ public class V3ManifestVerticle extends AbstractFesterVerticle {
                         width = Integer.parseInt(mediaWidth.get());
                         height = Integer.parseInt(mediaHeight.get());
                         image = new ImageContent(accessURI).setWidthHeight(width, height);
-                    } else if (isValidURL(pageURI)) {
+                    } else {
                         final ImageInfoLookup infoLookup = new ImageInfoLookup(pageURI);
 
                         width = infoLookup.getWidth();
                         height = infoLookup.getHeight();
                         image = new ImageContent(resourceURI).setServices(new ImageService2(pageURI));
-                    } else {
-                        throw new ImageNotFoundException(pageURI);
                     }
 
                     canvas.setWidthHeight(width, height).setThumbnails(new ImageContent(thumbnail));
@@ -591,7 +587,7 @@ public class V3ManifestVerticle extends AbstractFesterVerticle {
                 } catch (final ImageNotFoundException | IOException details) {
                     LOGGER.info(MessageCodes.MFS_078, pageID);
 
-                    if (aPlaceholderImage != null && isValidURL(aPlaceholderImage)) {
+                    if (aPlaceholderImage != null) {
                         try {
                             final ImageInfoLookup placeholderLookup = new ImageInfoLookup(aPlaceholderImage);
                             final int size;
@@ -645,21 +641,6 @@ public class V3ManifestVerticle extends AbstractFesterVerticle {
         }
 
         return canvases.toArray(new Canvas[] {});
-    }
-
-    /**
-     * Check for a valid URL.
-     *
-     * @param aURL A string which might be a valid URL
-     * @return True if the supplied string is a URL; else, false
-     */
-    private static boolean isValidURL(String aURL) {
-        try {
-            new URI(aURL).toURL();
-            return true;
-        } catch (MalformedURLException | URISyntaxException details) {
-            return false;
-        }
     }
 
     private VideoContent[] getVideoContent(final String aResourceURI) {
